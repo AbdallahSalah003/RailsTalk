ffc35e0bf6375acf0dc628e711312ae71cac5392
AbdallahSalah003 <abdallahsalahsalem9@gmail.com>
2025-10-03 02:30:33 +0300

    feat: auto-generate user_numbers and display it beside username in navbar

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  before_validation :set_user_numbers, if: -> { !user_numbers.present? }
+
+  def set_user_numbers
+    other_users_with_same_username = User.where("lower(username) like lower(?)", "%#{username}%")
+    self.user_numbers =  other_users_with_same_username.count + 1
+  end
 end
