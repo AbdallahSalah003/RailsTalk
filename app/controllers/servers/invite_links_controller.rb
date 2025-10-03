1d93b8eae818a9128aebaafa17085f0dece9cb24
AbdallahSalah003 <abdallahsalahsalem9@gmail.com>
2025-10-04 00:56:36 +0300

    Add server invitation and membership system with sidebar and routing updates

---
+++ app/controllers/servers/invite_links_controller.rb
@@ -0,0 +1,22 @@
+module Servers 
+  class InviteLinksController < ApplicationController
+    before_action :set_server, :authorize_owner!
+
+    def show  
+      @invite_link = @server.invite_links.find(params[:id])
+    end 
+    
+    def create 
+        @invite_link = @server.invite_links.create 
+        redirect_to server_invite_link_path(@server, @invite_link)
+    end 
+    
+  private 
+    def set_server 
+      @server = current_user.owned_servers.find(params[:server_id])
+    end 
+    def authorize_owner!
+      redirect_to root_path unless @server.owner == current_user 
+    end
+  end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,9 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   before_validation :set_user_numbers, if: -> { !user_numbers.present? }
   has_many :owned_servers, class_name: "Server", foreign_key: "owner_id"
+  has_many :server_memberships, class_name: "Server::Membership"
+  has_many :joined_servers, through: :server_memberships, source: :server
+
   def set_user_numbers
     other_users_with_same_username = User.where("lower(username) like lower(?)", "%#{username}%")
     self.user_numbers =  other_users_with_same_username.count + 1
