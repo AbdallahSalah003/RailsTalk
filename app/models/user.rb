86322c9711303cd7270094521fae89dd8304c8b5
AbdallahSalah003 <abdallahsalahsalem9@gmail.com>
2025-10-03 03:02:26 +0300

    feat: add Server model, controller, views, and initial CRUD setup

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   before_validation :set_user_numbers, if: -> { !user_numbers.present? }
-
+  has_many :owned_servers, class_name: "Server", foreign_key: "owner_id"
   def set_user_numbers
     other_users_with_same_username = User.where("lower(username) like lower(?)", "%#{username}%")
     self.user_numbers =  other_users_with_same_username.count + 1
